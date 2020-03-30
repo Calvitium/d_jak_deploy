6f0fa5ea2c7bbe097e40dfbcffb757ead167ff40
Calvitium <40168723+Calvitium@users.noreply.github.com>
2020-03-30 21:07:43 +0200

    Zadanie 1

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+# main.py
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/hello/{name}")
+async def read_item(name: str):
+    return f"Hello {name}"
