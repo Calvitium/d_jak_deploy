4b806850ebc4265cbd74d1837c167ce8641415cb
Calvitium <40168723+Calvitium@users.noreply.github.com>
2020-04-05 17:54:20 +0200

    Zadanie 2
    fixed path /method

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ app = FastAPI()
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.get("/GET")
+@app.get("/method")
 def root():
     return {"message": "GET"}
 
-@app.get("/POST")
+@app.put("/method")
 def root():
     return {"message": "POST"}
 
-@app.get("/PUT")
+@app.post("/method")
 def root():
     return {"message": "PUT"}
 
-@app.get("/DELETE")
+@app.delete("/method")
 def root():
     return {"message": "DELETE"}
 
