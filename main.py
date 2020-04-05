82e0cfbadc3fe1a4ca4865ff918cf540b29b28ce
Calvitium <40168723+Calvitium@users.noreply.github.com>
2020-04-05 18:18:05 +0200

    Zadanie 3

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+### TASK 1 ###########################################################
+
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+### TASK 2 ###########################################################
 
 @app.get("/method")
 def root():
@@ -24,6 +29,23 @@ def root():
 def root():
     return {"method": "DELETE"}
 
+### TASK 3 ###########################################################
+
+class GiveMeSomethingRq(BaseModel):
+	name: str
+	surename: str
+
+class GiveMeSomethingResp(BaseModel):
+	id: int
+	patient: Dict
+
+visited=0
+
+@app.post("/patient", response_model=GiveMeSomethingResp)
+def receive_patient(rq: GiveMeSomethingRq):
+	visited += 1
+	return GiveMeSomethingResp(id=visited, patient=rq.dict())
+	
 @app.get("/hello/{name}")
 async def read_item(name: str):
     return f"Hello {name}"
