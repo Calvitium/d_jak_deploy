9cf4aaa428ce3cf7bfc1cdf468d52abb0364f912
Calvitium <40168723+Calvitium@users.noreply.github.com>
2020-04-05 17:49:06 +0200

    Zadanie 2

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ app = FastAPI()
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
+@app.get("/GET")
+def root():
+    return {"message": "GET"}
+
+@app.get("/POST")
+def root():
+    return {"message": "POST"}
+
+@app.get("/PUT")
+def root():
+    return {"message": "PUT"}
+
+@app.get("/DELETE")
+def root():
+    return {"message": "DELETE"}
+
 @app.get("/hello/{name}")
 async def read_item(name: str):
     return f"Hello {name}"
