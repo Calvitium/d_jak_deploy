067c31feb7740e6d38a8edb13103edb254a1e510
Calvitium <40168723+Calvitium@users.noreply.github.com>
2020-04-24 15:44:30 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -96,10 +96,9 @@ def get_current_user(response: Response, credentials: HTTPBasicCredentials = Dep
     if not (correct_username and correct_password):
         raise HTTPException(status_code=401, detail="Incorrect email or password")
     session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
-    response = RedirectResponse(url='/welcome')	
     response.set_cookie(key="session_token", value=session_token)
     session_tokens.append(session_token)
-    return response
+    response.headers["Location"] = "/welcome"
 
 ### TASK 3 ###########################################################
 from fastapi import Cookie
