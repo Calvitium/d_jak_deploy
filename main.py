58a04dab86a089e1380c0357c6e2a352835d09a7
Calvitium <40168723+Calvitium@users.noreply.github.com>
2020-04-25 10:28:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -117,19 +117,19 @@ def logout(*, response: Response, session_token: str = Cookie(None)):
 
 ### TASK 5 ###########################################################
 @app.post("/patient")
-def add_patient(*, response: Response, patient: GiveMeSomethingRq, session_token: str = Cookie(None)):
+def add_patient(response: Response, patient: GiveMeSomethingRq, session_token: str = Cookie(None)):
 	if session_token not in app.session_tokens:
 		raise HTTPException(status_code=401, detail="Unathorised")
 	if app.ID not in app.patients.keys():
-		app.patients[app.ID] = rq.dict()
+		app.patients[app.ID] = patient.dict()
 		app.ID += 1
 	response.set_cookie(key="session_token", value=session_token)
 	response.headers["Location"] = "/patient/{id}"
 	response.status_code = status.HTTP_302_FOUND
     
 
-@app.post("/patient/{id}")
-def display_patient(*, response: Response, id: int, session_token: str = Cookie(None)):
+@app.get("/patient/{id}")
+def display_patient(response: Response, id: int, session_token: str = Cookie(None)):
 	if session_token not in app.session_tokens: 
 		raise HTTPException(status_code=401, detail="Unathorised")
 	response.set_cookie(key="session_token", value=session_token)
